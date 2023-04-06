808efe6d4717bcd8fa645e1fc579442712d8e960
Jeffrey Nguyen <beffrey@beffrey>
2023-04-06 16:15:55 -0700

    modified contents of portfolio

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import embedded from "./embedded.jpg"
+import meddevice from "./meddevice.jpg"
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -20,11 +22,23 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
+import ansible from "./tech/ansible.png";
+import jira from "./tech/atlassian-jira-logo-large.png";
+import aws from "./tech/aws.png";
+import ghactions from "./tech/ghactions.png";
+import k8s from "./tech/k8s.png";
+import linux from "./tech/linux.png";
+import office from "./tech/office.png";
+import postman from "./tech/postman.jpg";
+import python from "./tech/python.png";
+import terraform from "./tech/terraform.png"
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import station from "./company/station.png"
+import appliedmedical from "./company/appliedmedical.png"
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,4 +73,18 @@ export {
   carrent,
   jobit,
   tripguide,
+  station,
+  appliedmedical,
+  ansible,
+  jira,
+  aws,
+  ghactions,
+  k8s,
+  linux,
+  office,
+  postman,
+  python,
+  terraform,
+  embedded,
+  meddevice,
 };
